b09f27cc7ec6542818eb14e97cea1df69dd794ea
antonshawky22 <anton.shawky22@gmail.com>
2026-02-16 23:46:18 +0200

    Update multi-trend.py

--- multi-trend.py
+++ multi-trend.py
@@ -79,7 +79,7 @@ def rsi(series, period=14):
 # Parameters
 # =====================
 EMA_PERIOD = 60
-LOOKBACK = 10
+LOOKBACK = 80
 BULLISH_THRESHOLD = 0.65
 BEARISH_THRESHOLD = 0.65
 EMA_FORCED_SELL = 25
