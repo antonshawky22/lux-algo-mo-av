f7b707f0eead69890766db3fc84451c6b30a74d9
antonshawky22 <anton.shawky22@gmail.com>
2026-09-08 02:02:28 +0300

    Adjust MAX_RUNUP_PERCENT and buy3 price condition

--- backtest_multi_trend.py
+++ backtest_multi_trend.py
@@ -28,7 +28,7 @@ INITIAL_CAPITAL = 100000.0
 RSI_PERIOD = 14
 EMA100_PERIOD = 100
 RUNUP_LOOKBACK = 120
-MAX_RUNUP_PERCENT = 80
+MAX_RUNUP_PERCENT = 70
 MAX_GAP_DOWN_PERCENT = -5.0
 
 BUY1_RSI = 60
@@ -366,7 +366,7 @@ def backtest_stock(symbol, df):
             })
 
         # BUY L3
-        elif 0.65 < position < 1.0 and buy3 and price < avg_price * 0.92:
+        elif 0.65 < position < 1.0 and buy3 and price < avg_price * 0.93:
             old_pos = position
             state["position"] = 1.0
             state["avg_price"] = update_avg(avg_price, old_pos, price, state["position"])
