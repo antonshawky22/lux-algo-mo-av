c0eb194c5c650a4d9e84954dba11f8da006d15cb
antonshawky22 <anton.shawky22@gmail.com>
2026-09-08 14:28:00 +0300

    Refine EMA100 buy signal conditions
    Updated EMA100 conditions for buy signals.

--- backtest_multi_trend.py
+++ backtest_multi_trend.py
@@ -312,8 +312,8 @@ def backtest_stock(symbol, df):
         ema_up = (
             df["EMA100"].iloc[i] > df["EMA100"].iloc[i - 5] and
             df["EMA100"].iloc[i - 5] > df["EMA100"].iloc[i - 10] and
-            df["EMA100"].iloc[i] > df["EMA100"].iloc[i - 10] * 1.003 and
-            price <= df["EMA100"].iloc[i] * 1.08
+            df["EMA100"].iloc[i] > df["EMA100"].iloc[i - 10] * 1.002 and
+            price <= df["EMA100"].iloc[i] * 1.09
         )
 
         buy1 = safe_to_buy and ema_up and no_gap_down and rsi_val <= BUY1_RSI
