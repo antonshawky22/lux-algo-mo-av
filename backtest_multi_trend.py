160ea2f517ba2c7090d6e0679bec27e482fee541
antonshawky22 <anton.shawky22@gmail.com>
2026-09-08 15:53:23 +0300

    Adjust buy condition for L2 price threshold

--- backtest_multi_trend.py
+++ backtest_multi_trend.py
@@ -347,7 +347,7 @@ def backtest_stock(symbol, df):
             })
 
         # BUY L2
-        elif 0.32 < position < 0.50 and buy2 and price < avg_price * 0.96:
+        elif 0.32 < position < 0.50 and buy2 and price < avg_price * 0.98:
             old_pos = position
             state["position"] = 0.66
             state["avg_price"] = update_avg(avg_price, old_pos, price, state["position"])
